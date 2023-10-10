516c02aa036e957f1fa3eb43463beebebda9623e
Ritvik Mali <75354228+ritvikmali@users.noreply.github.com>
2023-10-10 17:48:02 +0530

    Update defines.php

--- instaAuto/defines.php
+++ instaAuto/defines.php
@@ -1,12 +1,12 @@
 <?php
-define('FACEBOOK_APP_ID', '1130187507844261');
-define('FACEBOOK_APP_SECRET', '658656bc718e17a1c758614bdb88b5eb');
+define('FACEBOOK_APP_ID', 'APP_ID');
+define('FACEBOOK_APP_SECRET', 'APP_SECERET');
 define('FACEBOOK_APP_VERSION', 'v18.0');
-define('FACEBOOK_REDIRECT_URI', 'http://loanbazaar.tech/');
+define('FACEBOOK_REDIRECT_URI', 'URI
 define('ENDPOINT_BASE', 'https://graph.facebook.com/v18.0/');
 
 // DB
-define('DB_SERVER', '192.168.0.128');
-define('DB_USERNAME', 'loanbazaar');
-define('DB_PASSWORD', 'Loan@Bazaar');
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
 define('DB_NAME', 'fb_db');
