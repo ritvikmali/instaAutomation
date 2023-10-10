afad5eaf6129b32384af3f1def8abb64a4538f44
Ritvik Mali <75354228+ritvikmali@users.noreply.github.com>
2023-10-10 17:45:49 +0530

    Add files via upload

---
+++ instaAuto/defines.php
@@ -0,0 +1,12 @@
+<?php
+define('FACEBOOK_APP_ID', '1130187507844261');
+define('FACEBOOK_APP_SECRET', '658656bc718e17a1c758614bdb88b5eb');
+define('FACEBOOK_APP_VERSION', 'v18.0');
+define('FACEBOOK_REDIRECT_URI', 'http://loanbazaar.tech/');
+define('ENDPOINT_BASE', 'https://graph.facebook.com/v18.0/');
+
+// DB
+define('DB_SERVER', '192.168.0.128');
+define('DB_USERNAME', 'loanbazaar');
+define('DB_PASSWORD', 'Loan@Bazaar');
+define('DB_NAME', 'fb_db');
